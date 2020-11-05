5920d3e3eb6fb5cb0be9cf1792684515f2ee191b
adumbidiot <nathaniel.daniel23@outlook.com>
2020-11-04 23:41:18 -0800

    Add blue example

---
+++ examples/blue.rs
@@ -0,0 +1,23 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use winapi::um::shellscalingapi::SetProcessDpiAwareness;
+use winapi::um::shellscalingapi::PROCESS_PER_MONITOR_DPI_AWARE;
+use windgi::primitives::Color;
+use windgi::primitives::Rect;
+use windgi::Brush;
+use windgi::DeviceContext;
+
+fn main() {
+    unsafe {
+        SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
+    }
+
+    let ctx = DeviceContext::desktop().expect("Failed to create DeviceContext");
+    let color = Color::new_rgb(0, 0, 255);
+    let brush = Brush::create_solid_brush(color).expect("Failed to create Solid Brush");
+
+    loop {
+        let rect = Rect::new_xywh(0, 0, 1920, 1080);
+        assert!(ctx.fill_rect(rect, &brush), "Failed to fill rect");
+    }
+}
